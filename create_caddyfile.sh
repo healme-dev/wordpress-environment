2dd68c3758047aae839b894519f13fb4427d0b3c
healme-dev <141905842+healme-dev@users.noreply.github.com>
2024-01-03 07:24:50 +0000

    update

--- create_caddyfile.sh
+++ create_caddyfile.sh
@@ -3,6 +3,6 @@
 echo "Enter your domain:"
 read domain
 
-sudo sed "s/{your_domain}/$domain/" /docker/wordpress-caddy-docker/caddy/Caddyfile.template | sudo tee /docker/wordpress-caddy-docker/caddy/Caddyfile > /dev/null
+sudo -u docker -i sed "s/{your_domain}/$domain/" /docker/wordpress-caddy-docker/caddy/Caddyfile.template | sudo -u docker -i tee /docker/wordpress-caddy-docker/caddy/Caddyfile > /dev/null
 
 echo "Caddyfile has been created with the domain: $domain"
